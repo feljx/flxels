d979d337bcf521a1bc49077dfdfebe92caff6315
flxsu <flxsu@users.noreply.github.com>
2019-06-03 02:04:39 +0200

    fix type declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,34 @@
-type view = () => HTMLElement
-declare class Context {
-	constructor (container: HTMLElement, views: view[])
-	container: HTMLElement
-	children: view[]
-	clear (): void
-	render (): void
-	clearView (view): void
-	renderView (view): void
+declare module 'flxels' {
+	type view = () => HTMLElement
+	declare class Context {
+		constructor (container: HTMLElement, views: view[])
+		container: HTMLElement
+		children: view[]
+		clear (): void
+		render (): void
+		clearView (view): void
+		renderView (view): void
+	}
+	type RenderingContext = Context
+	type StylesObject = { [key: string]: string | StylesObject }
+
+	declare function el (tag: string): HTMLElement
+
+	declare function styled (tag: string, styles: StylesObject): HTMLElement
+
+	declare function addClass (className: string, element: HTMLElement): void
+
+	declare function style (styles: StylesObject, element: HTMLElement): void
+
+	declare function restyle (styles: StylesObject, element: HTMLElement): void
+
+	declare function addText (text: string, element: HTMLElement): void
+
+	declare function div (styles: StylesObject): HTMLDivElement
+	declare function span (styles: StylesObject): HTMLSpanElement
+	declare function h1 (styles: StylesObject): HTMLHeadingElement
+	declare function h2 (styles: StylesObject): HTMLHeadingElement
+	declare function p (styles: StylesObject): HTMLParagraphElement
+	declare function input (styles: StylesObject): HTMLInputElement
+	declare function button (styles: StylesObject): HTMLButtonElement
 }
-type RenderingContext = Context
-type StylesObject = { [key: string]: string | StylesObject }
-
-declare function el (tag: string): HTMLElement
-
-declare function styled (tag: string, styles: StylesObject): HTMLElement
-
-declare function addClass (className: string, element: HTMLElement): void
-
-declare function style (styles: StylesObject, element: HTMLElement): void
-
-declare function restyle (styles: StylesObject, element: HTMLElement): void
-
-declare function addText (text: string, element: HTMLElement): void
-
-declare function div (styles: StylesObject): HTMLDivElement
-declare function span (styles: StylesObject): HTMLSpanElement
-declare function h1 (styles: StylesObject): HTMLHeadingElement
-declare function h2 (styles: StylesObject): HTMLHeadingElement
-declare function p (styles: StylesObject): HTMLParagraphElement
-declare function input (styles: StylesObject): HTMLInputElement
-declare function button (styles: StylesObject): HTMLButtonElement
